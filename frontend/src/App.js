2da5b62f28ea73c4e1baac226ea973e6f270b5c1
John Edward Phillips <je2phillips@uwaterloo.ca>
2021-07-03 13:12:37 -0400

    Autoformat App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Button from "@material-ui/core/Button";
 import axios from 'axios';
 import './App.css';
@@ -13,8 +13,8 @@ function App() {
 
   const getGreetings = (name, username, password) => {
     console.log("name = ", name)
-    axios.post("http://localhost:8080/v1/player/", {name, username, password}).then((res) => {
-      axios.get("http://localhost:8080/v1/player/", {params: {name}}).then((res) => {
+    axios.post("http://localhost:8080/v1/player/", { name, username, password }).then((res) => {
+      axios.get("http://localhost:8080/v1/player/", { params: { name } }).then((res) => {
         const greetingsData = res.data;
         setGreetings(greetingsData);
         console.log(greetingsData);
@@ -23,7 +23,7 @@ function App() {
   };
 
   const getWinStats = (moveString) => {
-    axios.get("http://localhost:8080/v1/game/winstats", {params: {moveString}}).then((res) => {
+    axios.get("http://localhost:8080/v1/game/winstats", { params: { moveString } }).then((res) => {
       setWinStats(res.data);
       console.log(res.data);
     });
@@ -33,77 +33,77 @@ function App() {
   return (
     <div className="App">
       <h1>chess!</h1>
-      Enter Name: 
-      <input 
-          type="text"
-          key="random1"
-          value={name}
-          placeholder="Enter Name Here"
-          onChange={(e) => setName(e.target.value)}
+      Enter Name:
+      <input
+        type="text"
+        key="random1"
+        value={name}
+        placeholder="Enter Name Here"
+        onChange={(e) => setName(e.target.value)}
       />
-      Enter Username: 
-      <input 
-          type="text"
-          key="random2"
-          value={username}
-          placeholder="Enter Username Here"
-          onChange={(e) => setUsername(e.target.value)}
+      Enter Username:
+      <input
+        type="text"
+        key="random2"
+        value={username}
+        placeholder="Enter Username Here"
+        onChange={(e) => setUsername(e.target.value)}
       />
-      Enter Password: 
-      <input 
-          type="text"
-          key="random3"
-          value={password}
-          placeholder="Enter Password Here"
-          onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-            onClick={() => {
-              getGreetings(name, username, password);
-            }}
-            variant="contained"
-            style={{
-              backgroundColor: "black",
-              borderRadius: 100,
-              color: "white",
-              textTransform: "none",
-            }}
-          >
-            Enter
-          </Button>
-        {greetings && 
-          <ul>
-            {greetings.map((value, index) => {
-              return <li key={index}>#{value.id} - Username: {value.username}, Name: {value.name} </li>
-            })}
-          </ul>
-        }
-      <input 
-          type="text"
-          key="random4"
-          value={moveString}
-          placeholder="Enter MoveString Here"
-          onChange={(e) => setMoveString(e.target.value)}
+      Enter Password:
+      <input
+        type="text"
+        key="random3"
+        value={password}
+        placeholder="Enter Password Here"
+        onChange={(e) => setPassword(e.target.value)}
       />
       <Button
-            onClick={() => {
-              getWinStats(moveString);
-            }}
-            variant="contained"
-            style={{
-              backgroundColor: "black",
-              borderRadius: 100,
-              color: "white",
-              textTransform: "none",
-            }}
-          >
-            Enter
-          </Button>
-        {winStats.numWhiteWins && 
-          <h3>
-            Num White Wins: {winStats.numWhiteWins} Num Black Wins: {winStats.numBlackWins} Num Games: {winStats.numGames}
-          </h3>
-        }
+        onClick={() => {
+          getGreetings(name, username, password);
+        }}
+        variant="contained"
+        style={{
+          backgroundColor: "black",
+          borderRadius: 100,
+          color: "white",
+          textTransform: "none",
+        }}
+      >
+        Enter
+      </Button>
+      {greetings &&
+        <ul>
+          {greetings.map((value, index) => {
+            return <li key={index}>#{value.id} - Username: {value.username}, Name: {value.name} </li>
+          })}
+        </ul>
+      }
+      <input
+        type="text"
+        key="random4"
+        value={moveString}
+        placeholder="Enter MoveString Here"
+        onChange={(e) => setMoveString(e.target.value)}
+      />
+      <Button
+        onClick={() => {
+          getWinStats(moveString);
+        }}
+        variant="contained"
+        style={{
+          backgroundColor: "black",
+          borderRadius: 100,
+          color: "white",
+          textTransform: "none",
+        }}
+      >
+        Enter
+      </Button>
+      {winStats.numWhiteWins &&
+        <h3>
+          Num White Wins: {winStats.numWhiteWins} Num Black Wins: {winStats.numBlackWins} Num Games: {winStats.numGames}
+        </h3>
+      }
     </div>
   );
 }
