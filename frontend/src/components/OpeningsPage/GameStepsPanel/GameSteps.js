3f5f5b3747d6520a240ee4dc1796e85c396a37ff
John Edward Phillips <je2phillips@uwaterloo.ca>
2021-07-04 15:59:35 -0400

    Group moves in pairs of two in GameSteps on Openings Tab

--- frontend/src/components/OpeningsPage/GameStepsPanel/GameSteps.js
+++ frontend/src/components/OpeningsPage/GameStepsPanel/GameSteps.js
@@ -7,18 +7,41 @@ function GameSteps({
 
     // var pastMoves = moves
     // pastMoves.pop()
+    const formatMoves = (moves) => {
+        let rows = [];
+        for (let i = 0; i < moves.length; ++i) {
+            if (i % 2 == 0) {
+                rows.push([moves[i].san]);
+            } else {
+                rows[rows.length - 1].push(moves[i].san);
+            }
+        }
+        let formattedRows = []
+        for (let i = 0; i < rows.length; ++i) {
+            if (rows[i].length == 1) {
+                formattedRows.push(<div>{(i + 1).toString() + ". " + rows[i][0]}</div>);
+            } else {
+                formattedRows.push(<div>{(i + 1).toString() + ". " + rows[i][0] + " " + rows[i][1]}</div>);
+            }
+        }
+
+        return (
+            <div>
+                {
+                    formattedRows
+                }
+            </div>
+        );
+
+    }
 
     return (
         <div className="game-steps-main-container">
             <h1 style={{ color: '#F2EDD4' }}>Game</h1>
             <div className="game-steps">
-                {moves.map((move, i) =>
-                    <div key={i}>
-                        <div>
-                            {move.san}
-                        </div>
-                    </div>
-                )}
+                {
+                    formatMoves(moves)
+                }
             </div>
         </div>
     );
