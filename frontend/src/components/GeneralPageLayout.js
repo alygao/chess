0f837ea6379e2ffe7c73b780c34b274810997de4
colinywu <colin.y.wu@gmail.com>
2021-07-04 17:29:31 -0400

    Minor change in routing with setState for each page

--- frontend/src/components/GeneralPageLayout.js
+++ frontend/src/components/GeneralPageLayout.js
@@ -41,6 +41,7 @@ function GeneralPageLayout() {
     setEventsPageShown(false);
     setIsSingleGameShown(false);
     setIsSingleEventShown(false);
+    setDefaultValue(-1);
   };
 
   const handleOpeningsPage = (e) => {
@@ -50,6 +51,7 @@ function GeneralPageLayout() {
     setEventsPageShown(false);
     setIsSingleGameShown(false);
     setIsSingleEventShown(false);
+    setDefaultValue(-1);
   };
 
   const handleEventsPage = (e) => {
@@ -59,6 +61,7 @@ function GeneralPageLayout() {
     setEventsPageShown(true);
     setIsSingleGameShown(false);
     setIsSingleEventShown(false);
+    setDefaultValue(-1);
   };
 
   return (
