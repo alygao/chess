4a969fbeb3a44c74c4b15c7830da8b8f267f1317
alyssagao1120 <alyssa.gao@yahoo.ca>
2021-08-06 16:51:29 -0400

    add extra sample data

--- database/insert_sample_data.sql
+++ database/insert_sample_data.sql
@@ -10,6 +10,7 @@ INSERT INTO `Game` VALUES (4, 'b', '2011-11-11', 2);
 INSERT INTO `Game` VALUES (5, 't', '2011-11-11', 1);
 INSERT INTO `Game` VALUES (6, 't', '2011-11-11', 2);
 
+-- Moves for Game 1
 INSERT INTO `Move` VALUES (1, 0, 'd4', 'very nice', 'd4', 'd2', 'P');
 INSERT INTO `Move` VALUES (1, 1, 'd5', 'wow', 'd5', 'd7', 'P');
 INSERT INTO `Move` VALUES (1, 2, 'c4', 'wow', 'c4', 'c2', 'P');
@@ -17,6 +18,7 @@ INSERT INTO `Move` VALUES (1, 3, 'e6', 'wow', 'e6', 'e7', 'P');
 INSERT INTO `Move` VALUES (1, 4, 'd3', 'wow', 'd3', 'd1', 'Q');
 INSERT INTO `Move` VALUES (1, 5, 'd7', 'wow', 'd7', 'e8', 'K');
 
+-- Moves for Game 2
 INSERT INTO `Move` VALUES (2, 0, 'd4', 'very nice', 'd4', 'd2', 'P');
 INSERT INTO `Move` VALUES (2, 1, 'd5', 'wow', 'd5', 'd7', 'P');
 INSERT INTO `Move` VALUES (2, 2, 'c4', 'wow', 'c4', 'c2', 'P');
@@ -24,6 +26,7 @@ INSERT INTO `Move` VALUES (2, 3, 'a5', 'nice', 'a5', 'a7', 'P');
 INSERT INTO `Move` VALUES (2, 4, 'f4', 'wow', 'f4', 'c1', 'B');
 INSERT INTO `Move` VALUES (2, 5, 'a7', 'wow', 'a7', 'a8', 'R');
 
+-- Moves for Game 3
 INSERT INTO `Move` VALUES (3, 0, 'a4', 'very nice', 'a4', 'a2', 'P');
 INSERT INTO `Move` VALUES (3, 1, 'h5', 'very nice', 'h5', 'h7', 'P');
 INSERT INTO `Move` VALUES (3, 2, 'a3', 'very nice', 'a3', 'a1', 'R');
