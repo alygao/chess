93bb695624a47060193bf4b81b677c42d58fd8ec
Jason Liu <jasonliu1999@gmail.com>
2021-08-10 17:05:40 -0400

    shitty frontend for login

--- frontend/src/components/GeneralPageLayout.js
+++ frontend/src/components/GeneralPageLayout.js
@@ -1,8 +1,10 @@
 import "./GeneralPageLayout.css";
+import axios from 'axios';
 
 import { useHistory, Link } from "react-router-dom";
 import { useState } from "react";
 import chessIcon from "../images/chess_icon.svg";
+import Button from '@material-ui/core/Button'
 
 import GeneralGamesPage from "./GamesPage/GeneralGamesPage";
 import OpeningsPage from "./OpeningsPage/OpeningsPage";
@@ -18,7 +20,10 @@ function GeneralPageLayout() {
   const [isSingleGameShown, setIsSingleGameShown] = useState(false);
   const [isSingleEvemtShown, setIsSingleEventShown] = useState(false);
   const [defaultValue, setDefaultValue] = useState(-1);
-  const [currUsername, setCurrUsername] = useState("alyssa123");
+  const [currUsername, setCurrUsername] = useState(null);
+
+  const [inputUsername, setInputUsername] = useState("")
+  const [inputPassword, setInputPassword] = useState("")
   
 
   // const history = useHistory();
@@ -78,6 +83,23 @@ function GeneralPageLayout() {
     setDefaultValue(-1);
   };
 
+  const handleLogin = (e) => {
+    e.preventDefault()
+    axios.post("http://localhost:8080/v1/player/login", 
+               {"username": inputUsername, "password":inputPassword}
+      ).then((res) => {
+        setCurrUsername(inputUsername);
+    }).catch((err) => {
+        alert("Incorrect username or password");
+    });
+
+  }
+
+  const logout = (e) => {
+    e.preventDefault()
+    setCurrUsername(null);
+  }
+
   return (
     <div className="general-page-main-container">
         <div className="general-page-menu-bar">
@@ -96,6 +118,38 @@ function GeneralPageLayout() {
               <li onClick={handleUsersPage}>Account</li>
             </Link>
           </div>
+          {currUsername  === null && <div className="general-page-menu-bar-right">
+            <input type="text" value={inputUsername} placeholder="Username" onChange={(e) => setInputUsername(e.target.value)}/>
+            <input type="text" value={inputPassword} placeholder="Password" onChange={(e) => setInputPassword(e.target.value)}/>
+          
+              
+            <Button
+              onClick={handleLogin}
+              variant="contained"
+              style={{
+                backgroundColor: "black",
+                borderRadius: 100,
+                color: "white",
+                textTransform: "none",
+              }}
+            >
+              Login
+            </Button>
+          </div>}
+          {currUsername && <div className="general-page-menu-bar-right">
+            <Button
+              onClick={logout}
+              variant="contained"
+              style={{
+                backgroundColor: "black",
+                borderRadius: 100,
+                color: "white",
+                textTransform: "none",
+              }}
+            >
+              Logout
+            </Button>
+          </div>}
         </div>
         <div className="general-page-content">
           {isGamesPageShown && <GeneralGamesPage isSingleGameShown={isSingleGameShown} setIsSingleGameShown={setIsSingleGameShown} defaultValue={defaultValue} currUsername={currUsername}/>}
